50a188dada5e1b9c5e555e50d03f5a95402d4731
Robert Prus <robert.prus87@gmail.com>
2019-11-23 17:47:39 +0100

    second commit

---
+++ src/main/java/com/hellspawn287/simple_credit_microservices/SimpleCreditMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.hellspawn287.simple_credit_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCreditMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCreditMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellspawn287/simple_credit_microservices/SimpleCreditMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellspawn287.simple_credit_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCreditMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
