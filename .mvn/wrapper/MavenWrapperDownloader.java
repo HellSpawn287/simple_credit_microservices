bc827ad5ab1b7be7b6b31853da4c7a22b1615540
Robert Prus <robert.prus87@gmail.com>
2019-11-24 23:33:57 +0100

    Small change in docker configuration in Pom

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,4 +1,3 @@
-/*
  * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -24,28 +24,32 @@ import java.util.Properties;
 
 public class MavenWrapperDownloader {
 
-    /**
+*
      * Default URL to download the maven-wrapper.jar from, if no 'downloadUrl' is provided.
-     */
+
+
     private static final String DEFAULT_DOWNLOAD_URL =
             "https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/0.4.2/maven-wrapper-0.4.2.jar";
 
-    /**
+*
      * Path to the maven-wrapper.properties file, which might contain a downloadUrl property to
      * use instead of the default one.
-     */
+
+
     private static final String MAVEN_WRAPPER_PROPERTIES_PATH =
             ".mvn/wrapper/maven-wrapper.properties";
 
-    /**
+*
      * Path where the maven-wrapper.jar will be saved to.
-     */
+
+
     private static final String MAVEN_WRAPPER_JAR_PATH =
             ".mvn/wrapper/maven-wrapper.jar";
 
-    /**
+*
      * Name of the property which should be used to override the default download url for the wrapper.
-     */
+
+
     private static final String PROPERTY_NAME_WRAPPER_URL = "wrapperUrl";
 
     public static void main(String args[]) {
