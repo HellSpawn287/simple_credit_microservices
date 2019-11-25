827db4629a021e0629463af506084848e42764a3
Robert Prus <robert.prus87@gmail.com>
2019-11-25 13:02:21 +0100

    Adding aplication.properties Adding aplication-localmysql.properties Adding mysql dependency to POM

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,19 +1,4 @@
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-
-
-import java.io.File;
+ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,6 +6,20 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.Properties;
+
+*Copyright 2012-2019the original author or authors.
+        *
+        *Licensed under the Apache License,Version2.0(the"License");
+        *you may not use this file except in compliance with the License.
+        *You may obtain a copy of the License at
+        *
+        *https://www.apache.org/licenses/LICENSE-2.0
+        *
+        *Unless required by applicable law or agreed to in writing,software
+        *distributed under the License is distributed on an"AS IS"BASIS,
+        *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+        *See the License for the specific language governing permissions and
+        *limitations under the License.
 
 public class MavenWrapperDownloader {
 
